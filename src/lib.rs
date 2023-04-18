3b621442777819a97345f0cf02a1dbb8d7aaf0eb
Jonathan ARNOULT <jonathan.arnoult@polytechnique.edu>
2023-04-18 12:09:26 -0700

    Add compile() function to interface

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@
 #![warn(clippy::missing_docs_in_private_items)]
 
 use ast::{Program, Stratum};
+use compile::Compiler;
 use typing::Typer;
 
 pub mod ast;
@@ -19,6 +20,9 @@ extern crate quote;
 
 /// Checks a given program.
 ///
+/// If it returns successfully, the program type-checked. Otherwise, it will panic (note: temporary behavior,
+/// there should of course be no panicking in the future, only `Result`s).
+///
 /// You must provide a closure/function that takes the id of the static stratum
 /// and returns a program.
 ///
@@ -26,4 +30,12 @@ extern crate quote;
 pub fn check(mut p: impl FnMut(Stratum) -> Program) {
     let mut typer = Typer::new();
     typer.check(&p(typer.static_stratum()));
+}
+
+/// Compiles a given program.
+///
+/// Under the hood, in charge of allocating a new `Compiler` and launching it on your program.
+pub fn compile(p: Program) -> String {
+    let mut compiler = Compiler::new();
+    compiler.compile(p)
 }
