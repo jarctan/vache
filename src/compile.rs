954ee2dc24f00a5b4afc84f91e21a634d90dbd95
Jonathan ARNOULT <jonathan.arnoult@polytechnique.edu>
2023-05-11 14:13:37 -0700

    Compile our first program from a CFG repr!

--- src/compile.rs
+++ src/compile.rs
@@ -4,7 +4,7 @@ use proc_macro2::TokenStream;
 use string_builder::Builder as StringBuilder;
 use Ty::*;
 
-use crate::mir::{Program, Ty};
+use crate::mir::{Fun, Instr, Program, RValue, Struct, Ty, VarMode};
 
 /// Compiler, that turns our language into source code for an
 /// executable language.
@@ -18,6 +18,10 @@ impl Compiler {
     /// Producing the necessary prelude for all our outputs.
     fn prelude() -> TokenStream {
         quote!(
+            #![allow(clippy::needless_late_init)]
+            #![allow(clippy::unused_unit)]
+            #![allow(unused_mut)]
+
             extern crate rug;
             use std::borrow::{Cow, Borrow};
             use std::ops::{Sub, Add, Rem, Mul, Div};
@@ -106,7 +110,7 @@ impl Compiler {
 
             /// Prelude function.
             #[allow(clippy::ptr_arg)]
-            pub(crate) fn __clone<'a, B: ?Sized + ToOwned>(cow: &'a Cow<'a, B>) -> Cow<'a, B> {
+            pub(crate) fn __borrow<'a, B: ?Sized + ToOwned>(cow: &'a Cow<'a, B>) -> Cow<'a, B> {
                 match *cow {
                     Cow::Borrowed(b) => Cow::Borrowed(b),
                     Cow::Owned(ref o) => {
@@ -131,7 +135,7 @@ impl Compiler {
     }
 
     /// Translate a type into a Rust type.
-    pub fn translate_type(&self, ty: Ty, show_lifetime: bool) -> TokenStream {
+    pub fn translate_type(&self, ty: &Ty, show_lifetime: bool) -> TokenStream {
         match ty {
             UnitT => quote!(()),
             BoolT => quote!(bool),
@@ -156,8 +160,178 @@ impl Compiler {
         }
     }
 
+    /// Compiles a struct.
+    fn visit_struct(&mut self, strukt: Struct) -> TokenStream {
+        let name = format_ident!("{}", strukt.name);
+        let fields: TokenStream = strukt
+            .fields
+            .into_iter()
+            .map(|(k, ty)| {
+                let k = format_ident!("{k}");
+                let ty = self.translate_type(&ty, true);
+                quote!(#k: #ty,)
+            })
+            .collect();
+        quote!(
+            #[derive(Debug, Clone)]
+            pub struct #name<'a> {
+                #fields
+            }
+        )
+    }
+
+    /// Compiles a variable (with its addressing mode).
+    fn visit_var(&mut self, var: VarMode) -> TokenStream {
+        let name = format_ident!("{}", var.var.as_str());
+        if var.owned {
+            quote!(#name.clone())
+        } else {
+            quote!(__borrow(&#name))
+        }
+    }
+
+    /// Compiles a right value.
+    fn visit_rvalue(&mut self, rvalue: RValue) -> TokenStream {
+        match rvalue {
+            RValue::Unit => quote!(()),
+            RValue::Integer(i) => {
+                let i = i.to_f64();
+                quote!(Cow::Owned(::rug::Integer::from_f64(#i).unwrap()))
+            }
+            RValue::String(s) => {
+                quote!(Cow::Owned(::std::string::String::from(#s)))
+            }
+            RValue::Var(v) => self.visit_var(v),
+            RValue::Field(s, field) => {
+                let s = self.visit_var(s);
+                let field = format_ident!("{field}");
+                quote!(#s.#field)
+            }
+        }
+    }
+
+    /// Compiles a single CFG instruction.
+    fn visit_instr(&mut self, instr: &Instr) -> TokenStream {
+        match instr {
+            crate::mir::Instr::Noop => quote!(),
+            crate::mir::Instr::Declare(v) => {
+                let name = format_ident!("{}", v.name.as_str());
+                let ty = self.translate_type(&v.ty, false);
+                quote! {
+                    let mut #name: #ty;
+                }
+            }
+            crate::mir::Instr::Assign(lhs, rhs) => {
+                let lhs = format_ident!("{}", lhs.as_str());
+                let rhs = self.visit_rvalue(rhs.clone());
+                quote! {
+                    #lhs = #rhs;
+                }
+            }
+            crate::mir::Instr::Call {
+                name,
+                args,
+                destination,
+            } => {
+                let destination = format_ident!("{}", destination.name.as_str());
+                if name == "print" {
+                    let mut builder = StringBuilder::default();
+
+                    // Compute the format string
+                    let mut args_iter = args.iter();
+                    if args_iter.next().is_some() {
+                        builder.append("{}");
+                    }
+                    for _ in args_iter {
+                        builder.append(" {}");
+                    }
+
+                    let args = args.iter().map(|arg| self.visit_var(arg.clone()));
+                    let fmt_str = builder.string().unwrap();
+                    quote!(let #destination = println!(#fmt_str, #(#args),*);)
+                } else {
+                    let args = args.iter().map(|arg| self.visit_var(arg.clone()));
+                    let name = match name.as_str() {
+                        "+" => "__add".to_string(),
+                        "-" => "__sub".to_string(),
+                        "*" => "__mul".to_string(),
+                        "/" => "__div".to_string(),
+                        "%" => "__rem".to_string(),
+                        "<" => "__lt".to_string(),
+                        ">" => "__gt".to_string(),
+                        "<=" => "__le".to_string(),
+                        ">=" => "__ge".to_string(),
+                        "==" => "__eq".to_string(),
+                        "!=" => "__neq".to_string(),
+                        _ => name.to_string(),
+                    };
+                    let name = format_ident!("{name}");
+                    quote!(let #destination = #name(#(#args),*);)
+                }
+            }
+            crate::mir::Instr::Struct { .. } => todo!(),
+            crate::mir::Instr::Branch(_) => todo!(),
+            crate::mir::Instr::PushScope => quote! {
+                {}
+            },
+            crate::mir::Instr::PopScope => quote!({}),
+        }
+    }
+
+    /// Compiles a function.
+    fn visit_fun(&mut self, f: Fun) -> TokenStream {
+        let name = format_ident!("{}", f.name);
+        let params: Vec<TokenStream> = f
+            .params
+            .into_iter()
+            .map(|param| {
+                let name = format_ident!("{}", String::from(param.name));
+                let ty = self.translate_type(&param.ty, true);
+                quote! {
+                    #name: #ty
+                }
+            })
+            .collect();
+
+        let body: TokenStream = f
+            .body
+            .bfs(&f.entry_l)
+            .map(|(_, instr)| self.visit_instr(instr))
+            .collect();
+        let ty = self.translate_type(&f.ret_v.ty, true);
+
+        let ret_v = format_ident!("{}", f.ret_v.name.as_str());
+
+        if params.is_empty() {
+            quote! {
+                pub fn #name(#(#params),*) -> #ty {
+                    let #ret_v;
+                    #body;
+                    #ret_v
+                }
+            }
+        } else {
+            quote! {
+                pub fn #name<'a>(#(#params),*) -> #ty {
+                    let #ret_v;
+                    #body;
+                    #ret_v
+                }
+            }
+        }
+    }
+
     /// Compiles a program.
     pub fn visit_program(&mut self, p: Program) -> TokenStream {
-        todo!()
+        let Program { funs, structs } = p;
+        let funs: Vec<TokenStream> = funs.into_values().map(|f| self.visit_fun(f)).collect();
+        let structs: Vec<TokenStream> = structs
+            .into_values()
+            .map(|s| self.visit_struct(s))
+            .collect();
+        quote! {
+            #(#structs)*
+            #(#funs)*
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 //! Toy Stratum language compiler.
 
-use vache_lib::{borrow_check, check, interp, mir};
+use vache_lib::{borrow_check, check, compile, mir};
 
 fn main() {
     let checked = check(vache_lib::examples::borrows());
     let mir = mir(checked);
     let mir = borrow_check(mir);
-    println!("{:#?}", mir);
-    println!("Output: {}", interp(mir));
+    println!("Output: {}", compile(mir));
 }
