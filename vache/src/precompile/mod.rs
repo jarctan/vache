2162e3506f29fd53e7e97f9dfb05b1e67b8a6bf5
Jonathan ARNOULT <jonathan.arnoult@polytechnique.edu>
2023-05-19 14:39:28 -0700

    Enhance precompile_instr

--- vache/src/precompile/mod.rs
+++ vache/src/precompile/mod.rs
@@ -35,24 +35,29 @@ fn precompile_fun(f: mir::Fun) -> Fun {
 
 /// Pre-compiles a CFG.
 fn precompile_cfg(cfg: mir::Cfg, entry_l: mir::CfgLabel) -> Vec<Stmt> {
-    let body = precompile_instr(
+    let (branch, body) = precompile_instr(
         Stratum::static_stm(),
         &mut cfg.into_cfs(entry_l).peekable(),
         false,
     );
+    assert_eq!(branch, Branch::default());
     body
 }
 
 /// Pre-compiles instructions in the CFG.
-fn precompile_instr(scope: Stratum, iter: &mut Peekable<CfsSelf>, in_loop: bool) -> Vec<Stmt> {
+fn precompile_instr(
+    scope: Stratum,
+    iter: &mut Peekable<CfsSelf>,
+    in_loop: bool,
+) -> (Branch, Vec<Stmt>) {
     let mut body = vec![];
-    let branch = iter.peek().map(|(b, _, _)| b).cloned().unwrap_or_default();
+    let initial_branch = iter.peek().map(|(b, _, _)| b).cloned().unwrap_or_default();
 
     while let Some((_, is_loop, i)) = iter.peek() {
         let is_loop = *is_loop;
         match scope.cmp(&i.scope) {
             Ordering::Less => body.push({
-                let stmts = precompile_instr(unsafe { scope.incr() }, iter, is_loop);
+                let (_, stmts) = precompile_instr(unsafe { scope.incr() }, iter, is_loop);
                 if is_loop {
                     Stmt::Loop(stmts)
                 } else {
@@ -64,7 +69,7 @@ fn precompile_instr(scope: Stratum, iter: &mut Peekable<CfsSelf>, in_loop: bool)
                 if in_loop {
                     body.push(Stmt::Break);
                 }
-                return body;
+                return (initial_branch, body);
             }
             Ordering::Equal => {
                 let (_, _, i) = iter.next().unwrap();
@@ -82,13 +87,21 @@ fn precompile_instr(scope: Stratum, iter: &mut Peekable<CfsSelf>, in_loop: bool)
                         destination,
                     }),
                     InstrKind::Branch(v) => {
-                        let iftrue = precompile_instr(scope, iter, in_loop);
-                        let iffalse = precompile_instr(scope, iter, in_loop);
-                        body.push(Stmt::If(v, iftrue, iffalse));
+                        let (b1, stmts1) = precompile_instr(scope, iter, in_loop);
+                        let (b2, stmts2) = precompile_instr(scope, iter, in_loop);
+                        match (&b1, &b2) {
+                            (Branch::TrueB, Branch::FalseB) => {
+                                body.push(Stmt::If(v, stmts1, stmts2))
+                            }
+                            (Branch::FalseB, Branch::TrueB) => {
+                                body.push(Stmt::If(v, stmts2, stmts1))
+                            }
+                            _ => panic!("Unexpected control flow {b1:?} {b2:?}"),
+                        }
                     }
                 }
             }
         }
     }
-    body
+    (initial_branch, body)
 }
